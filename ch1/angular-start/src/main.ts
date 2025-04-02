33c60ed8ea752e8c3670b2cdf301f7ee02fda844
Bob Zhang <bobzhang55@yahoo.com>
2025-04-02 14:20:04 -0500

    test my github

--- ch1/angular-start/src/main.ts
+++ ch1/angular-start/src/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+//just a comments
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));

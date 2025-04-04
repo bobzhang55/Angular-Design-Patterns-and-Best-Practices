063441bebaa1b5e73278d2b8073fbe047e9a3df1
Bob Zhang <bobzhang55@yahoo.com>
2025-04-03 21:45:38 -0500

    Create shared module for common imports.

--- ch2/my/src/app/home/home.module.ts
+++ ch2/my/src/app/home/home.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { SharedModule } from '../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 
 @NgModule({
@@ -16,14 +10,8 @@ import { MatListModule } from '@angular/material/list';
     HomeComponent
   ],
   imports: [
-    CommonModule,
-    HomeRoutingModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule
+    SharedModule,
+     HomeRoutingModule,
   ]
 })
 export class HomeModule { }

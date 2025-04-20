6c60d0d1ef5a39e81c2b599568eb417176236515
Bob Zhang <bobzhang55@yahoo.com>
2025-04-20 12:46:23 -0500

    kill ts error

--- ch9/gym-diary/src/app/diary/entry-item/entry-item.component.ts
+++ ch9/gym-diary/src/app/diary/entry-item/entry-item.component.ts
@@ -7,15 +7,15 @@ import { ExerciseSet } from '../interfaces/exercise-set';
   styleUrls: ['./entry-item.component.css'],
 })
 export class EntryItemComponent {
-  @Input('exercise-set') exerciseSet!: ExerciseSet;
+  @Input('exercise-set') exerciseSet!: ExerciseSet | undefined;
   @Output() editEvent = new EventEmitter<ExerciseSet>();
   @Output() deleteEvent = new EventEmitter<string>();
 
   delete() {
-    this.deleteEvent.emit(this.exerciseSet.id);
+    this.deleteEvent.emit(this.exerciseSet?.id);
   }
 
   editEntry() {
-    this.editEvent.emit(this.exerciseSet);
+    this.editEvent.emit(this.exerciseSet ? this.exerciseSet : undefined);
   }
 }
